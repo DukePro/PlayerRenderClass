6721a82a860af174bb02b65fb1f26162e09db344
D Egor <Mail.egor.d@gmail.com>
2022-11-15 14:14:47 +0300

    +

--- PlayerRenderClass/Program.cs
+++ PlayerRenderClass/Program.cs
@@ -15,16 +15,16 @@
         public char _playerChar { get; private set; }
         private int _health { get; set; }
         private int _damage { get; set; }
-        public int _playerPositionX { get; private set; }
-        public int _playerPositionY { get; private set; }
+        public int PlayerPositionX { get; private set; }
+        public int PlayerPositionY { get; private set; }
 
         public Player(char playerChar, int health, int damage, int playerPositionX, int playerPositionY)
         {
             _playerChar = playerChar;
             _health= health;
             _damage= damage;
-            _playerPositionX = playerPositionX;
-            _playerPositionY = playerPositionY;
+            PlayerPositionX = playerPositionX;
+            PlayerPositionY = playerPositionY;
         }
 
         public Player()
@@ -32,8 +32,8 @@
             _playerChar = (char)02;
             _health = 100;
             _damage = 20;
-            _playerPositionX = 5;
-            _playerPositionY = 5;
+            PlayerPositionX = 5;
+            PlayerPositionY = 5;
         }
     }
 
@@ -41,7 +41,7 @@
     {
         public void DrawPlayerIcon(Player player)
         {
-            Console.SetCursorPosition(player._playerPositionX, player._playerPositionY);
+            Console.SetCursorPosition(player.PlayerPositionX, player.PlayerPositionY);
             Console.Write(player._playerChar);
         }
     }
